825033a0b82a76751295093d831d9df43802a475
Jason Antalis <jason.antalis@gmail.com>
2022-05-12 22:35:28 +0200

    💄 Added back button functionality on driver-home

--- libs/client/pages/src/lib/driver-home/driver-home.tsx
+++ libs/client/pages/src/lib/driver-home/driver-home.tsx
@@ -1,16 +1,33 @@
+import { Center } from 'native-base';
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { DriverHomeProps } from '../NavigationTypes/navigation-types';
 
 /* eslint-disable-next-line */
-export interface DriverHomeProps {}
 
-export function DriverHome(props: DriverHomeProps) {
+export function DriverHome({ navigation }: DriverHomeProps) {
   return (
-    <View>
-      <Text>Welcome to driver-home!</Text>
-    </View>
+    <SafeAreaView >
+      <View style={{
+        left: 5 , margin: 10
+      }}>
+        <Icon
+          name="arrow-left"
+          size={30}
+          style={{
+          }}
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <View>
+        <Center>
+          <Text>Trip Details</Text>
+        </Center>
+      </View>
+    </SafeAreaView>
   );
 }
-
 export default DriverHome;

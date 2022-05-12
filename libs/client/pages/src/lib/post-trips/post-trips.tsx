a55ef32ce5afe9b6e46aebbb4d4be3255a6f8f8b
Jason Antalis <jason.antalis@gmail.com>
2022-05-12 13:26:57 +0200

    💄 (Client): Creation of Trips Page

---
+++ libs/client/pages/src/lib/post-trips/post-trips.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+/* eslint-disable-next-line */
+export interface PostTripsProps {}
+
+export function PostTrips(props: PostTripsProps) {
+  return (
+    <View>
+      <Text>Welcome to post-trips!</Text>
+    </View>
+  );
+}
+
+export default PostTrips;

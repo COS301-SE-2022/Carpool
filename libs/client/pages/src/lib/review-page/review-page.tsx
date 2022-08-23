1d184710a5290168c0950d473c60057082b39881
Ashleigh-Tuks <93780371+Ashleigh-Tuks@users.noreply.github.com>
2022-08-23 08:09:38 +0200

    👽(Reviews): Created Review Page

---
+++ libs/client/pages/src/lib/review-page/review-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReviewPageProps } from '../NavigationTypes/navigation-types';
+import { RatingCard, } from '@carpool/client/components';
+
+export function ReviewPage({ navigation }: ReviewPageProps) {
+
+  return (
+    <RatingCard />
+
+);
+}
+
+export default ReviewPage;
+

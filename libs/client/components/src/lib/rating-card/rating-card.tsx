7727407e71064893734ac1fe8b82bcfe54c9cbcd
Ashleigh-Tuks <93780371+Ashleigh-Tuks@users.noreply.github.com>
2022-08-24 09:47:42 +0200

    👽(Rating): Update to Rating Card

--- libs/client/components/src/lib/rating-card/rating-card.tsx
+++ libs/client/components/src/lib/rating-card/rating-card.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line */
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { AirbnbRating } from 'react-native-ratings';
 
 
@@ -8,92 +8,102 @@ export function RatingCard(){
   const ratingCompleted = (rating:number) => {
     console.log("Rating is: " + rating)
   }
-
-  const submitHandler = () => {
-    console.log("Where disptach for rating goes")
-    // dispatch(login({ email, password }));
-  };
   return (
-    <View style={[styles.flexCol, styles.userContainer]}>
-    <View
-      style={[
-        styles.flexRow,
-        {
-          flex: 1,
-        },
-      ]}
-    >
-      <View
-        style={[
-          styles.flexRow,
-          {
-            flex: 1,
-            justifyContent: 'flex-start',
-            paddingHorizontal: 20,
-            paddingVertical: 15,
-            borderRadius: 15,
-            marginVertical: 10,
-            backgroundColor: '#fff',
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-
-          },
-        ]}
-      >
+    <View>
+      <View style={styles.row}>
         <Image
-          source={require('./lighter_grey.png')}
-          resizeMode="contain"
-          style={styles.image}
-        />
-        <View>
-          <Text
-            style={[
-              styles.textLargeBlack,
-              {
-                marginBottom: 5,
-              },
-            ]}
-          >
-            How was your driving with Benjamin?
-          </Text>
-          <Text style={styles.textMediumLight}>Tuesday morning to University of Pretoria</Text>
-          <AirbnbRating
-            count={5}
-            defaultRating={3}
-            size={30}
-            showRating={false}
-            onFinishRating={ratingCompleted}
-            />
-            <TouchableOpacity
-        onPress={submitHandler}
-        style={[styles.container,]}>
-        <Text style={styles.text}>Rate</Text>
-      </TouchableOpacity>
-        </View>
+            source={require('./lighter_grey.png')}
+            resizeMode="contain"
+            style={styles.image}
+          />
+        <Text style={styles.textLargeBlack}>
+          How was your trip with Benjamin?
+        </Text>
+        <Text style={styles.textMediumLight}>
+          Tuesday morning to University of Pretoria
+        </Text>
       </View>
+
+      <AirbnbRating
+        count={5}
+        defaultRating={3}
+        size={20}
+        showRating={false}
+        onFinishRating={ratingCompleted}
+      />
+
+
     </View>
-  </View>
+  //   <View style={[styles.flexCol, styles.userContainer]}>
+  //   <View
+  //     style={[
+  //       styles.flexRow,
+  //       {
+  //         flex: 1,
+  //       },
+  //     ]}
+  //   >
+  //     <View
+  //       style={[
+  //         styles.flexRow,
+  //         {
+  //           flex: 1,
+  //           justifyContent: 'flex-start',
+  //           paddingHorizontal: 20,
+  //           paddingVertical: 15,
+  //           borderRadius: 15,
+  //           marginVertical: 10,
+  //           backgroundColor: '#fff',
+  //           shadowColor: '#000',
+  //           shadowOffset: {
+  //             width: 0,
+  //             height: 2,
+  //           },
+  //           shadowOpacity: 0.25,
+  //           shadowRadius: 3.84,
+  //           elevation: 5,
+
+  //         },
+  //       ]}
+  //     >
+  //       <Image
+  //         source={require('./lighter_grey.png')}
+  //         resizeMode="contain"
+  //         style={styles.image}
+  //       />
+  //       <View>
+  //         <Text
+  //           style={[
+  //             styles.textLargeBlack,
+  //             {
+  //               marginBottom: 5,
+  //             },
+  //           ]}
+  //         >
+  //           How was your trip with Benjamin?
+  //         </Text>
+  //         <Text style={styles.textMediumLight}>Tuesday morning to University of Pretoria</Text>
+  //         <AirbnbRating
+  //           count={5}
+  //           defaultRating={3}
+  //           size={20}
+  //           showRating={false}
+  //           onFinishRating={ratingCompleted}
+  //           // ratingContainerStyle={{left: '-10%'}}
+  //           />
+  //     <TouchableOpacity
+  //       onPress={submitHandler}
+  //       style={[styles.container,]}>
+  //       <Text style={styles.text}>Rate</Text>
+  //     </TouchableOpacity>
+  //       </View>
+  //     </View>
+  //   </View>
+  // </View>
 );
 }
 
 const styles = StyleSheet.create({
-  flexRow: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  flexCol: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
   shadow: {
     shadowColor: '#000',
     shadowOffset: {
@@ -103,11 +113,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+
   },
-  userContainer: {
-    flex: 1,
-    backgroundColor: '#f8f8f8',
-    padding: 20,
+  row:{
+    flexDirection: "row",
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
   },
   image: {
     width: 50,
@@ -116,12 +127,15 @@ const styles = StyleSheet.create({
     marginRight: 15,
     borderWidth: 3,
     borderColor: '#188aed',
+    float: 'left',
   },
   textLargeBlack: {
     fontWeight: '600',
     fontSize: 16,
   },
   textMediumLight: {
+    marginLeft: '20%',
+    marginTop:'-8%',
     fontSize: 11,
     fontWeight: '400',
     color: '#808080',
@@ -129,17 +143,31 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: '#188aed',
-    width: '30%',
+    width: '100%',
     padding: 10,
     marginTop: 10,
     borderRadius: 15,
-    left: '70%',
+    // left: '-10%'
   },
   text: {
     color: '#fff',
-    fontSize: 12,
+    fontSize: 14,
     textAlign: 'center',
     fontWeight: '500',
+  },
+  Containershadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    backgroundColor: '#fff',
+    padding: 20,
+    margin: 10,
+    borderRadius: 25,
   },
 
 });
